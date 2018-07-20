d8075ebf91bf94bfe005fc7247be553223173a12
Vladimir Alekseev <a.vovka@gmail.com>
2018-07-20 12:39:21 +0000

    add tests

---
+++ tests/BaseTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Test
+{
+
+    class BaseTest extends \PHPUnit\Framework\TestCase
+    {
+        public function testBase()
+        {
+            $this->assertEquals(1, 1);
+        }
+    }
+}
